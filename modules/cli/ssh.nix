b8dfe0cb94153af68091f5f193ab4eb94e314c04
Jason Cox <steel300@gmail.com>
2023-11-01 12:59:34 -0500

    Use 1password for ssh authentication

--- modules/cli/ssh.nix
+++ modules/cli/ssh.nix
@@ -6,6 +6,11 @@
     enable = true;
     includes = ["~/.1password/ssh_config"];
 
+    extraConfig = ''
+      Host *
+        IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+      '';
+
     matchBlocks = {
       net = {
         forwardAgent = true;
