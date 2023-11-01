c737f387b3c5713e4fdb6f987d90fbc8ee586a09
Jason Cox <steel300@gmail.com>
2023-11-01 14:27:35 -0500

    feat(brew): Add more packages to be managed by nix-darwin's brew module

--- modules/brew.nix
+++ modules/brew.nix
@@ -5,5 +5,12 @@
       { name = "gpg"; }
       { name = "pinentry-mac"; }
     ];
+
+    casks = [
+      { name = "firefox"; }
+      { name = "1password"; }
+      { name = "iterm2"; }
+      { name = "keepingyouawake";  }
+    ];
   };
 }
